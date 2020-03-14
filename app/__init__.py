53b8dbfcbb7930eeed340d98f1eccf281e9da5bb
romaine murray <romainemurray6@gmail.com>
2020-03-14 18:51:37 -0500

    bug fixes

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from os import urandom
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = sha3_512(urandom(24)).hexdigest()
+app.config['SECRET_KEY'] = sha3_512(urandom(12)).hexdigest()
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://xihulcjmmabslq:57e4eac48762e39558cc8872238cc817d2acbfceeab62737af25e03d6d9a7830@ec2-23-22-156-110.compute-1.amazonaws.com:5432/dedc0l9a6nh70r'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
